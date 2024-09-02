47f5f3e7b3164fae2f05ab4ac08ba87dab0739b5
aarish200 <169061837+aarish200@users.noreply.github.com>
2024-09-03 00:43:32 +0530

    Update app.py

--- projectify2 2/projectify2/app.py
+++ projectify2 2/projectify2/app.py
@@ -408,4 +408,5 @@ def counselor(user_id, chosen_project):
         return "Sorry, an error occurred while generating the advice."
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5004)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
